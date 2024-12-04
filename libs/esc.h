6949fe3ba093ccb606cd191920cf6582beec6a71
filipembedded <filipstojanovic.mas@gmail.com>
2024-12-04 14:22:14 +0100

    Added spi1 and i2c1 nodes overlays

--- libs/esc.h
+++ libs/esc.h
@@ -13,12 +13,31 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/pwm.h>
 
+/*********************************** Defines **********************************/
+// ESC Protocol Duty Cycle range in us
+#define DC_MIN_PWM_US     1000
+#define DC_MAX_PWM_US     2000
+
+#define DC_MIN_ONESHOT_125_US 125
+#define DC_MAX_ONESHOT_125_US 250
+
+#define DC_MIN_ONESHOT_42_US  42
+#define DC_MAX_ONESHOT_42_US  84
+
+#define DC_MIN_MULTISHOT_US   5
+#define DC_MAX_MULTISHOT_US   25
+
+
+// ESC Protocol Period in us
+#define T_PWM_US 4000
+
 /*********************************** Typedefs *********************************/
 typedef int status_t;
 
 
 typedef enum {
-    ESC_ONESHOT_125 = 0,
+    ESC_PWM = 0,
+    ESC_ONESHOT_125,
     ESC_ONESHOT_42,
     ESC_MULTISHOT
 } esc_protocol_t;
--- src/main.c
+++ src/main.c
@@ -4,21 +4,23 @@
 
 
 /* PWM configuration */
-#define PWM_PERIOD_US     100 /* Period in microseconds */
-#define PWM_DUTY_CYCLE_US 50  /* DT in microseconds */
+#define FREQ_2_PERIOD_S(freq) (1/freq)
+#define PERIOD_S_2_PERIOD_US (period_s) (period_s/1000000)
+
+#define FREQUENCY         500
+
+#define PWM_PERIOD_US     500/* Period in microseconds */
+#define PWM_DUTY_CYCLE_US 125  /* DT in microseconds */
+
+
 
 int main(void)
 {
     /* Get the PWM device */
     const struct device *pwm_dev_1 = DEVICE_DT_GET(DT_NODELABEL(pwm1));
-    const struct device *pwm_dev_2 = DEVICE_DT_GET(DT_NODELABEL(pwm2));
-    const struct device *pwm_dev_3 = DEVICE_DT_GET(DT_NODELABEL(pwm3));
-    const struct device *pwm_dev_4 = DEVICE_DT_GET(DT_NODELABEL(pwm4));
 
-    if (!device_is_ready(pwm_dev_1) ||
-        !device_is_ready(pwm_dev_2) ||
-        !device_is_ready(pwm_dev_3) ||
-        !device_is_ready(pwm_dev_4))
+    /* Check if device is ready */
+    if (!device_is_ready(pwm_dev_1))
     {
         printk("Error: PWM device is not ready\n");
         return;
@@ -27,28 +29,31 @@ int main(void)
     printk("PWM device is ready!\n");
     
     /* Setting the PWM signal */
-    int ret1, ret2, ret3, ret4;
+    int ret1, ret2, ret3, ret4, dt_inc;
+    dt_inc = 0;
 
     ret1 = pwm_set(pwm_dev_1,
                   1, 
                   PWM_USEC(PWM_PERIOD_US),
                   PWM_USEC(PWM_DUTY_CYCLE_US),
                   0);
-    ret2 = pwm_set(pwm_dev_2,
+    ret1 = pwm_set(pwm_dev_1,
                   2, 
                   PWM_USEC(PWM_PERIOD_US),
                   PWM_USEC(PWM_DUTY_CYCLE_US),
                   0);
-    ret3 = pwm_set(pwm_dev_3,
+    ret3 = pwm_set(pwm_dev_1,
                   3, 
                   PWM_USEC(PWM_PERIOD_US),
                   PWM_USEC(PWM_DUTY_CYCLE_US),
                   0);
-    ret4 = pwm_set(pwm_dev_4,
+    ret4 = pwm_set(pwm_dev_1,
                   4, 
                   PWM_USEC(PWM_PERIOD_US),
                   PWM_USEC(PWM_DUTY_CYCLE_US),
                   0);
+
+    
     
     if (ret1 || ret2 || ret3 || ret4)
     {
@@ -58,6 +63,15 @@ int main(void)
 
     while (1)
     {
-        //k_msleep(1000);
+        pwm_set(pwm_dev_1,
+                1,
+                PWM_USEC(PWM_PERIOD_US),
+                PWM_USEC(dt_inc),
+                0);
+        if (dt_inc >= PWM_PERIOD_US)
+            dt_inc = 0;
+        else 
+            dt_inc++;
+        k_msleep(5);
     }
 }
