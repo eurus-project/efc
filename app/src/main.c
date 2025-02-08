fd1d7a7850283fe577d04fafa39bd0b1f11a6b92
Djordje Nedic <nedic.djordje2@gmail.com>
2025-02-08 23:23:35 +0100

    refactor: Split into threads and establish ZBUS communication
    This splits most of the current logic, notably the logger and telemetry
    packer into separate threads with adequate priorities, and establishes
    IPC using ZBUS.

--- app/src/main.c
+++ app/src/main.c
@@ -27,24 +27,27 @@
 #include <zephyr/logging/log.h>
 #include <zephyr/usb/usb_device.h>
 #include <zephyr/usb/usbd.h>
+#include <zephyr/zbus/zbus.h>
 
-#include "common/mavlink.h"
-#include "ulog.h"
-#include "ulog_accel.h"
-#include "ulog_altitude.h"
-#include "ulog_baro.h"
-#include "ulog_gyro.h"
-
+#include "logger.h"
 #include "radio_receiver.h"
+#include "telemetry_packer.h"
 #include "telemetry_sender.h"
 
-#define ALTITUDE_SOURCE_TYPE_BARO 1
+#include "common/mavlink.h"
 
-// Value derrived from BMP280 datasheet (page 15 of 49) for the given sensor
-// configuration.
-#define BMP280_ALTITUDE_VARIANCE_M .017f
+#include "types.h"
 
 LOG_MODULE_REGISTER(main);
+
+ZBUS_OBS_DECLARE(logger_sub);
+ZBUS_OBS_DECLARE(telemetry_packer_sub);
+
+ZBUS_CHAN_DEFINE(imu_chan, struct imu_6dof_data, NULL, NULL,
+                 ZBUS_OBSERVERS(logger_sub, telemetry_packer_sub), {0});
+
+ZBUS_CHAN_DEFINE(baro_chan, struct baro_data, NULL, NULL,
+                 ZBUS_OBSERVERS(logger_sub, telemetry_packer_sub), {0});
 
 // This LED simply blinks at an interval, indicating visually that the firmware
 // is running If anything causes the whole firmware to abort, it will be
@@ -52,13 +55,13 @@ LOG_MODULE_REGISTER(main);
 static const struct gpio_dt_spec fw_running_led =
     GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
 
-static const uint8_t telemetry_system_id = 0;
-static const uint8_t telemetry_component_id = MAV_COMP_ID_AUTOPILOT1;
-static const uint8_t telemetry_channel_ground = 0;
+const uint8_t telemetry_system_id = 0;
+const uint8_t telemetry_component_id = MAV_COMP_ID_AUTOPILOT1;
+const uint8_t telemetry_channel_ground = MAVLINK_COMM_0;
 
 static struct fs_littlefs main_fs;
 
-static struct fs_mount_t main_fs_mount = {
+struct fs_mount_t main_fs_mount = {
     .type = FS_LITTLEFS,
     .fs_data = &main_fs,
     .flags = FS_MOUNT_FLAG_USE_DISK_ACCESS,
@@ -66,23 +69,25 @@ static struct fs_mount_t main_fs_mount = {
     .mnt_point = "/SD:",
 };
 
+uint32_t boot_count = 0;
+
 struct k_pipe telemetry_ground_pipe;
 static uint8_t telemetry_ground_pipe_data[1024];
 
 static mavlink_message_t mavlink_msg;
 static uint8_t mavlink_ser_buf[MAVLINK_MAX_PACKET_LEN];
 
-static ULOG_Inst_Type ulog_log;
-static uint16_t gyro_msg_id = 0;
-static uint16_t accel_msg_id = 0;
-static uint16_t baro_msg_id = 0;
-static uint16_t baro_alt_msg_id = 0;
-
 K_THREAD_STACK_DEFINE(radio_thread_stack, 1024);
 static struct k_thread radio_thread;
 
-K_THREAD_STACK_DEFINE(telemetry_thread_stack, 2048);
-static struct k_thread telemetry_thread;
+K_THREAD_STACK_DEFINE(telemetry_packer_thread_stack, 2048);
+static struct k_thread telemetry_packer_thread;
+
+K_THREAD_STACK_DEFINE(telemetry_sender_thread_stack, 1024);
+static struct k_thread telemetry_sender_thread;
+
+K_THREAD_STACK_DEFINE(logger_thread_stack, 8192);
+static struct k_thread logger_thread;
 
 #if defined(CONFIG_USB_DEVICE_STACK_NEXT)
 static struct usbd_context *sample_usbd;
@@ -105,10 +110,6 @@ static int enable_usb_device_next(void) {
 #endif /* defined(CONFIG_USB_DEVICE_STACK_NEXT) */
 
 static int process_imu(const struct device *dev) {
-    struct sensor_value temperature;
-    struct sensor_value accel[3];
-    struct sensor_value gyro[3];
-
     int ret = sensor_sample_fetch(dev);
     if (ret < 0) {
         LOG_ERR("Could not fetch data from IMU!");
@@ -117,122 +118,78 @@ static int process_imu(const struct device *dev) {
 
     const int64_t timestamp_ms = k_uptime_get();
 
+    struct sensor_value accel[3];
     ret = sensor_channel_get(dev, SENSOR_CHAN_ACCEL_XYZ, accel);
     if (ret < 0) {
         LOG_ERR("Could not get accelerometer data!");
         return ret;
     }
 
+    struct sensor_value gyro[3];
     ret = sensor_channel_get(dev, SENSOR_CHAN_GYRO_XYZ, gyro);
     if (ret < 0) {
         LOG_ERR("Could not get gyroscope data!");
         return ret;
     }
 
+    struct sensor_value temperature;
     ret = sensor_channel_get(dev, SENSOR_CHAN_DIE_TEMP, &temperature);
     if (ret < 0) {
         LOG_WRN("Could not get IMU die temperature data!");
     }
 
-    mavlink_msg_scaled_imu_pack_chan(
-        telemetry_system_id, telemetry_component_id, telemetry_channel_ground,
-        &mavlink_msg, timestamp_ms, sensor_value_to_milli(&accel[0]),
-        sensor_value_to_milli(&accel[1]), sensor_value_to_milli(&accel[2]),
-        sensor_value_to_milli(&gyro[0]), sensor_value_to_milli(&gyro[1]),
-        sensor_value_to_milli(&gyro[2]), 0, 0, 0,
-        sensor_value_to_milli(&temperature) / 10);
+    const struct imu_6dof_data msg = {
+        .timestamp_us = timestamp_ms * 1000,
+        .accel_mps2[0] = sensor_value_to_float(&accel[0]),
+        .accel_mps2[1] = sensor_value_to_float(&accel[1]),
+        .accel_mps2[2] = sensor_value_to_float(&accel[2]),
+        .gyro_radps[0] = sensor_value_to_float(&gyro[0]),
+        .gyro_radps[1] = sensor_value_to_float(&gyro[1]),
+        .gyro_radps[2] = sensor_value_to_float(&gyro[2]),
+        .temperature_degc = sensor_value_to_float(&temperature),
+    };
 
-    const uint16_t telemetry_msg_len =
-        mavlink_msg_to_send_buffer(mavlink_ser_buf, &mavlink_msg);
-
-    ret = k_pipe_write(&telemetry_ground_pipe, mavlink_ser_buf,
-                       telemetry_msg_len, K_NO_WAIT);
-
-    if (ret < 0) {
-        LOG_WRN("Could not fit data into telemetry pipe!");
+    ret = zbus_chan_pub(&imu_chan, &msg, K_NO_WAIT);
+    if (ret < 0 && ret != -EAGAIN && ret != -EBUSY) {
+        LOG_ERR("Failed to send imu message on zbus!");
     }
-
-    ULOG_Gyro_Type gyro_msg = {
-        .timestamp = timestamp_ms * 1000,
-        .x = sensor_value_to_float(&gyro[0]),
-        .y = sensor_value_to_float(&gyro[1]),
-        .z = sensor_value_to_float(&gyro[2]),
-    };
-
-    ULOG_Accel_Type accel_msg = {
-        .timestamp = timestamp_ms * 1000,
-        .x = sensor_value_to_float(&accel[0]),
-        .y = sensor_value_to_float(&accel[1]),
-        .z = sensor_value_to_float(&accel[2]),
-    };
-
-    ULOG_Gyro_Write(&ulog_log, &gyro_msg, gyro_msg_id);
-    ULOG_Accel_Write(&ulog_log, &accel_msg, accel_msg_id);
 
     return 0;
 }
 
 static int process_baro(const struct device *dev) {
-    struct sensor_value baro_temp;
-    struct sensor_value baro_press;
-
-    float temperature;
-    float pressure;
-    float altitude;
-
     int ret = sensor_sample_fetch(dev);
     if (ret < 0) {
         LOG_ERR("Could not fetch data from barometer!");
         return ret;
     }
 
+    const uint32_t timestamp_ms = k_uptime_get();
+
+    struct sensor_value baro_temp;
     ret = sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &baro_temp);
     if (ret < 0) {
         LOG_ERR("Could not get barometer temperature data!");
         return ret;
     }
 
+    struct sensor_value baro_press;
     ret = sensor_channel_get(dev, SENSOR_CHAN_PRESS, &baro_press);
     if (ret < 0) {
         LOG_ERR("Could not get barometer pressure data!");
         return ret;
     }
 
-    temperature = sensor_value_to_float(&baro_temp);
-    pressure = sensor_value_to_float(&baro_press);
-    const uint32_t timestamp_ms = k_uptime_get();
-
-    ULOG_Baro_Type baro_msg = {
-        .timestamp = timestamp_ms * 1000,
-        .temperature = temperature,
-        .pressure = pressure,
+    const struct baro_data msg = {
+        .timestamp_us = timestamp_ms * 1000,
+        .temperature_degc = sensor_value_to_float(&baro_temp),
+        .pressure_kpa = sensor_value_to_float(&baro_press),
     };
 
-    mavlink_msg_scaled_pressure_pack_chan(
-        telemetry_system_id, telemetry_component_id, telemetry_channel_ground,
-        &mavlink_msg, timestamp_ms, pressure * 10.0f, 0.0f,
-        sensor_value_to_milli(&baro_temp) / 10, 0);
-
-    const uint16_t telemetry_msg_len =
-        mavlink_msg_to_send_buffer(mavlink_ser_buf, &mavlink_msg);
-
-    ret = k_pipe_write(&telemetry_ground_pipe, mavlink_ser_buf,
-                       telemetry_msg_len, K_NO_WAIT);
-
-    if (ret < 0) {
-        LOG_WRN("Could not fit data into telemetry pipe!");
+    ret = zbus_chan_pub(&baro_chan, &msg, K_NO_WAIT);
+    if (ret < 0 && ret != -EAGAIN && ret != -EBUSY) {
+        LOG_ERR("Failed to send baro message on zbus!");
     }
-
-    ULOG_Baro_Write(&ulog_log, &baro_msg, baro_msg_id);
-
-    altitude = 44330.0f * (1.0f - powf((pressure / 101.325f), 1.0f / 5.255f));
-
-    ULOG_Altitude_Type altitude_msg = {.timestamp = timestamp_ms * 1000,
-                                       .source = ALTITUDE_SOURCE_TYPE_BARO,
-                                       .altitude = altitude,
-                                       .variance = BMP280_ALTITUDE_VARIANCE_M};
-
-    ULOG_Altitude_Write(&ulog_log, &altitude_msg, baro_alt_msg_id);
 
     return 0;
 }
@@ -301,7 +258,6 @@ int main(void) {
         return 0;
     }
 
-    uint32_t boot_count = 0;
     struct fs_file_t boot_count_file;
     fs_file_t_init(&boot_count_file);
 
@@ -341,94 +297,26 @@ int main(void) {
 
     LOG_INF("Boot count: %d\n", (int)boot_count);
 
-    memset(filename, 0, sizeof(filename));
-    snprintf(filename, sizeof(filename), "%s/log_%d.ulg",
-             main_fs_mount.mnt_point, (int)boot_count);
-    ULOG_Config_Type log_cfg = {
-        .filename = filename,
-    };
+    k_pipe_init(&telemetry_ground_pipe, telemetry_ground_pipe_data,
+                sizeof(telemetry_ground_pipe_data));
 
     k_thread_create(&radio_thread, radio_thread_stack,
                     K_THREAD_STACK_SIZEOF(radio_thread_stack), radio_receiver,
                     NULL, NULL, NULL, 0, 0, K_NO_WAIT);
 
-    k_pipe_init(&telemetry_ground_pipe, telemetry_ground_pipe_data,
-                sizeof(telemetry_ground_pipe_data));
-
-    k_thread_create(&telemetry_thread, telemetry_thread_stack,
-                    K_THREAD_STACK_SIZEOF(telemetry_thread_stack),
+    k_thread_create(&telemetry_sender_thread, telemetry_sender_thread_stack,
+                    K_THREAD_STACK_SIZEOF(telemetry_sender_thread_stack),
                     telemetry_sender, NULL, NULL, NULL,
-                    K_LOWEST_APPLICATION_THREAD_PRIO, 0, K_NO_WAIT);
-    k_thread_start(&telemetry_thread);
+                    K_LOWEST_APPLICATION_THREAD_PRIO - 2, 0, K_NO_WAIT);
 
-    if (ULOG_Init(&ulog_log, &log_cfg) != ULOG_SUCCESS) {
-        LOG_ERR("Could not open log!");
-    }
+    k_thread_create(&telemetry_packer_thread, telemetry_packer_thread_stack,
+                    K_THREAD_STACK_SIZEOF(telemetry_packer_thread_stack),
+                    telemetry_packer, NULL, NULL, NULL,
+                    K_LOWEST_APPLICATION_THREAD_PRIO - 1, 0, K_NO_WAIT);
 
-    if (ULOG_Gyro_RegisterFormat(&ulog_log) != ULOG_SUCCESS) {
-        LOG_ERR("Could not register ULOG gyro format!");
-    }
-
-    if (ULOG_Accel_RegisterFormat(&ulog_log) != ULOG_SUCCESS) {
-        LOG_ERR("Could not register ULOG accel format!");
-    }
-
-    if (ULOG_Baro_RegisterFormat(&ulog_log) != ULOG_SUCCESS) {
-        LOG_ERR("Could not register ULOG baro format!");
-    }
-
-    if (ULOG_Altitude_RegisterFormat(&ulog_log) != ULOG_SUCCESS) {
-        LOG_ERR("Could not register ULOG altitude format!");
-    }
-
-    const char alt_src_type_baro_key[] = "int32_t ALTITUDE_SOURCE_TYPE_BARO";
-    const int32_t alt_src_type_baro = ALTITUDE_SOURCE_TYPE_BARO;
-    if (ULOG_AddParameter(&ulog_log, alt_src_type_baro_key,
-                          strlen(alt_src_type_baro_key), &alt_src_type_baro)) {
-        LOG_ERR("Could not write Altitude Source Type Baro parameter!");
-    }
-
-    const char sys_name_key[] = "char[3] sys_name";
-    const char sys_name[] = "EFC";
-    if (ULOG_AddInfo(&ulog_log, sys_name_key, strlen(sys_name_key), sys_name,
-                     strlen(sys_name))) {
-        LOG_ERR("Could not system name info to the log!");
-    }
-
-    const char main_imu_name_key[] = "char[7] main_imu_name";
-    const char main_imu_name[] = "MPU6050";
-    if (ULOG_AddInfo(&ulog_log, main_imu_name_key, strlen(main_imu_name_key),
-                     main_imu_name, strlen(main_imu_name))) {
-        LOG_ERR("Could not main IMU info to the log!");
-    }
-
-    const char main_baro_name_key[] = "char[6] main_baro_name";
-    const char main_baro_name[] = "BMP280";
-    if (ULOG_AddInfo(&ulog_log, main_baro_name_key, strlen(main_baro_name_key),
-                     main_baro_name, strlen(main_baro_name))) {
-        LOG_ERR("Could not write main baro info to the log!");
-    }
-
-    if (ULOG_StartDataPhase(&ulog_log) != ULOG_SUCCESS) {
-        LOG_ERR("Could not start ULOG data phase!");
-    }
-
-    if (ULOG_Gyro_Subscribe(&ulog_log, 0, &gyro_msg_id) != ULOG_SUCCESS) {
-        LOG_ERR("Could not subscribe ULOG log to gyro message!");
-    }
-
-    if (ULOG_Accel_Subscribe(&ulog_log, 0, &accel_msg_id) != ULOG_SUCCESS) {
-        LOG_ERR("Could not subscribe ULOG log to accel message!");
-    }
-
-    if (ULOG_Baro_Subscribe(&ulog_log, 0, &baro_msg_id) != ULOG_SUCCESS) {
-        LOG_ERR("Could not subscribe ULog baro message!");
-    }
-
-    if (ULOG_Altitude_Subscribe(&ulog_log, 0, &baro_alt_msg_id) !=
-        ULOG_SUCCESS) {
-        LOG_ERR("Could not subscribe ULog altitude message!");
-    }
+    k_thread_create(&logger_thread, logger_thread_stack,
+                    K_THREAD_STACK_SIZEOF(logger_thread_stack), logger, NULL,
+                    NULL, NULL, K_LOWEST_APPLICATION_THREAD_PRIO, 0, K_NO_WAIT);
 
     while (1) {
         ret = gpio_pin_toggle_dt(&fw_running_led);
@@ -440,8 +328,6 @@ int main(void) {
         process_imu(main_imu);
 
         process_baro(main_baro);
-
-        ULOG_Sync(&ulog_log);
 
         telemetry_heartbeat();
 
