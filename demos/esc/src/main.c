2fc5e1c4a2668c1d2162fa21712dda8f609d9e14
filipembedded <filipembedded@gmail.com>
2025-02-12 00:49:25 +0100

    feat(esc): Add Electronic Speed Controller - ESC support

---
+++ demos/esc/src/main.c
@@ -0,0 +1,138 @@
+/*
+ * This file is part of the efc project <https://github.com/eurus-project/efc/>.
+ * Copyright (c) 2024 - 2025 The efc developers.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <math.h>
+#include <stdio.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/pwm.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/usb/usbd.h>
+
+#include "esc.h"
+
+LOG_MODULE_REGISTER(main);
+
+#define ESC_SPEED_CHANGE_MS 1000
+#define ESC_ARMED_INDICATOR_MS 100
+
+// This LED simply blinks at an interval, indicating visually that the firmware
+// is running If anything causes the whole firmware to abort, it will be
+// apparent without looking at log output or hooking up a debugger.
+static const struct gpio_dt_spec fw_running_led =
+    GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+
+#if defined(CONFIG_USB_DEVICE_STACK_NEXT)
+static struct usbd_context *sample_usbd;
+
+static int enable_usb_device_next(void) {
+    int err;
+
+    sample_usbd = sample_usbd_init_device(NULL);
+    if (sample_usbd == NULL) {
+        return -ENODEV;
+    }
+
+    err = usbd_enable(sample_usbd);
+    if (err) {
+        return err;
+    }
+
+    return 0;
+}
+#endif /* defined(CONFIG_USB_DEVICE_STACK_NEXT) */
+
+int main(void) {
+    if (DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart)) {
+#if defined(CONFIG_USB_DEVICE_STACK_NEXT)
+        if (enable_usb_device_next()) {
+            return 0;
+        }
+#else
+        if (usb_enable(NULL)) {
+            return 0;
+        }
+#endif
+    }
+
+    if (!gpio_is_ready_dt(&fw_running_led)) {
+        LOG_ERR("The firmware running LED is not ready");
+        return 0;
+    }
+
+    int ret = gpio_pin_configure_dt(&fw_running_led, GPIO_OUTPUT_ACTIVE);
+    if (ret < 0) {
+        LOG_ERR("Could not configure the firmware running LED as output");
+        return 0;
+    }
+
+    /* ESC Initialization */
+    ESC_Error_Type status;
+    ESC_Inst_Type esc1;
+    ESC_Protocol_Type protocol;
+
+#if CONFIG_ESC_PWM
+    protocol = ESC_PWM;
+#elif CONFIG_ESC_ONESHOT_125
+    protocol = ESC_ONESHOT_125;
+#elif CONFIG_ESC_ONESHOT_42
+    protocol = ESC_ONESHOT_42;
+#elif CONFIG_ESC_MULTISHOT
+    protocol = ESC_MULTISHOT;
+#endif
+
+    const struct device *pwm_dev = DEVICE_DT_GET(DT_NODELABEL(pwm1));
+
+    if (!device_is_ready(pwm_dev))
+        return;
+
+    status = ESC_Init(&esc1, pwm_dev, 1, protocol);
+    if (status < 0)
+        return;
+
+    /* Arming procedure */
+    printk("ESC Arming...\n");
+
+    status = ESC_Arm(&esc1);
+    if (status < 0) {
+        return;
+    } else {
+        printk("ESC Armed.\n");
+        for (int i = 0; i < 10; i++) {
+            gpio_pin_toggle_dt(&fw_running_led);
+            k_msleep(ESC_ARMED_INDICATOR_MS);
+        }
+    }
+
+    while (1) {
+
+        for (float i = 0.0f; i < 1.0f; i += 0.1f) {
+            gpio_pin_toggle_dt(&fw_running_led);
+            ESC_SetSpeed(&esc1, i);
+            k_msleep(ESC_SPEED_CHANGE_MS);
+        }
+        for (float i = 1.0f; i > 0.0f; i -= 0.1f) {
+            gpio_pin_toggle_dt(&fw_running_led);
+            ESC_SetSpeed(&esc1, i);
+            k_msleep(ESC_SPEED_CHANGE_MS);
+        }
+    }
+
+    return 0;
+}
---
+++ libs/motor_control/esc/esc.h
@@ -0,0 +1,114 @@
+/*
+ * This file is part of the efc project <https://github.com/eurus-project/efc/>.
+ * Copyright (c) 2024-2025 The efc developers.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ESC_H
+#define ESC_H
+
+#include <stdio.h>
+#include <zephyr/drivers/pwm.h>
+#include <zephyr/kernel.h>
+
+typedef enum {
+    ESC_SUCCESS = 0,
+    ESC_DEVICE_PWM_NOT_READY,
+    ESC_INVALID_PROTOCOL,
+    ESC_INVALID_SPEED_SET,
+    ESC_TIMER_OVERFLOW_ERROR,
+    ESC_NOT_INITIALIZED,
+    ESC_INVALID_STOP,
+    ESC_INVALID_ARMING,
+} ESC_Error_Type;
+
+typedef enum {
+    ESC_PWM = 0,
+    ESC_ONESHOT_125,
+    ESC_ONESHOT_42,
+    ESC_MULTISHOT
+} ESC_Protocol_Type;
+
+typedef struct {
+    const struct device *pwm_dev;
+    uint32_t pwm_channel;
+    uint32_t period_us;
+    float min_pulse_duration_us;
+    float max_pulse_duration_us;
+    ESC_Protocol_Type protocol;
+    bool initialized;
+} ESC_Inst_Type;
+
+/**
+ * @brief ESC Initialization function
+ * @param[in] pwm_dev     Pointer to the device struct - pwm device
+ * @param[in] pwm_channel PWM channel which will be used by ESC device
+ * @param[in] protocol    ESC Protocol
+ * @param[out] esc_out    Pointer to the ESC out struct, it needs to be passed
+ *                        to the ESC_SetSpeed function
+ *
+ * @retval ESC_SUCCESS - Operation finished successfully
+ * @retval ESC_DEVICE_PWM_NOT_READY - PWM device is not initialized correctly
+ * @retval ESC_NOT_INITIALIZED - Specified esc is not initialized correctly
+ */
+ESC_Error_Type ESC_Init(ESC_Inst_Type *esc_out, const struct device *pwm_dev,
+                        const uint32_t pwm_channel,
+                        const ESC_Protocol_Type protocol);
+
+/**
+ * @brief ESC Set Speed by percentage from 0 to 1
+ * @param[in] esc   Pointer to the preinitialized ESC out struct from ESC_Init
+ * @param[in] speed ESC speed value [0.0f,1.0f]
+ *
+ * @retval ESC_SUCCESS - Operation finished successfully
+ * @retval ESC_INVALID_SPEED_SET - Speed is out of valid range [0-100]
+ * @retval ESC_TIMER_OVERFLOW_ERROR - Timer value is overflowed, check prescaler
+ * @retval ESC_NOT_INITIALIZED - Specified esc is not initialized correctly
+ */
+ESC_Error_Type ESC_SetSpeed(const ESC_Inst_Type *esc, const float speed);
+
+/**
+ * @brief ESC Stop - Stop ESC signal
+ * @param[in] esc Pointer to the preinitialized ESC out struct from ESC_Init
+ *
+ * @retval ESC_SUCCESS - Operation finished successfully
+ * @retval ESC_NOT_INITIALIZED - Specified esc is not initialized correctly
+ * @retval ESC_INVALID_STOP - Error in ESC stop command
+ */
+ESC_Error_Type ESC_Stop(const ESC_Inst_Type *esc);
+
+/**
+ * @brief ESC Deinitialize device
+ * @param[in] esc Pointer to the preinitialized ESC out struct from ESC_Init
+ *
+ * @retval ESC_SUCCESS - Operation finished successfully
+ * @retval ESC_NOT_INITIALIZED - Specified esc is not initialized correctly
+ */
+ESC_Error_Type ESC_DeInit(ESC_Inst_Type *esc);
+
+/**
+ * @brief ESC Arming procedure
+ * @param[in] esc Pointer to the preinitialized ESC out struct from ESC_Init
+ *
+ * @note This function blocks the thread it is called in because of use of
+ * k_msleep!
+ *
+ * @retval ESC_SUCCESS - Operation finished successfully
+ * @retval ESC_INVALID_PROTOCOL - Specified ESC protocol is not valid
+ * @retval ESC_NOT_INITIALIZED - Specified esc is not initialized correctly
+ * @retval ESC_INVALID_ARMING - Error in arming procedure
+ */
+ESC_Error_Type ESC_Arm(const ESC_Inst_Type *esc);
+
+#endif
