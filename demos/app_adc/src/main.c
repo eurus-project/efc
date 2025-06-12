4b823755427bd6bd421db2efb5735040cd4f3792
filipembedded <filipembedded@gmail.com>
2025-06-12 20:29:57 +0200

    Refactored ADC example

--- demos/app_adc/src/main.c
+++ demos/app_adc/src/main.c
@@ -5,44 +5,66 @@
 #include <zephyr/usb/usb_device.h>
 #include <zephyr/usb/usbd.h>
 
+LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+
 static const struct adc_dt_spec adc_channel =
     ADC_DT_SPEC_GET(DT_PATH(zephyr_user));
 
-void main(void) {
+#if defined(CONFIG_USB_DEVICE_STACK_NEXT)
+static struct usbd_context *sample_usbd;
+
+static int enable_usb_device_next(void) {
     int err;
-    int16_t buf;
+
+    sample_usbd = sample_usbd_init_device(NULL);
+    if (sample_usbd == NULL) {
+        return -ENODEV;
+    }
+
+    err = usbd_enable(sample_usbd);
+    if (err) {
+        return err;
+    }
+
+    return 0;
+}
+#endif /* defined(CONFIG_USB_DEVICE_STACK_NEXT) */
+
+int main(void) {
+    int err;
+    int32_t buf = 0;
     int32_t value_mV;
     uint16_t sample;
 
     if (DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart)) {
 #if defined(CONFIG_USB_DEVICE_STACK_NEXT)
         if (enable_usb_device_next()) {
-            return;
+            return 0;
         }
 #else
         if (usb_enable(NULL)) {
-            return;
+            return 0;
         }
 #endif
     }
 
     if (!adc_is_ready_dt(&adc_channel)) {
         printk("ADC device not ready\n");
-        return;
+        return 0;
     }
 
     err = adc_channel_setup_dt(&adc_channel);
     if (err < 0) {
         printk("Could not setup channel!\n");
-        return;
+        return 0;
     }
 
     struct adc_sequence sequence = {
         .buffer = &buf,
-        /* buffer size in bytes, not number of samples */
-        .buffer_size = sizeof(buf),
+        // buffer size in bytes, not number of samples
+        .buffer_size = 2,
         // Optional
-        //.calibrate = true,
+        .calibrate = true,
     };
 
     err = adc_sequence_init_dt(&adc_channel, &sequence);
@@ -57,12 +79,13 @@ void main(void) {
             printk("Could not read (%d)", err);
             continue;
         } else {
-            sample = sequence.buffer;
-            printk("Raw ADC value: %d", sample);
-            err = adc_raw_to_millivolts_dt(&adc_channel, &value_mV);
-            printk("Value in millivolts: %d", value_mV);
+            printk("Raw ADC value: %d", buf);
+            err = adc_raw_to_millivolts_dt(&adc_channel, &buf);
+            printk("Value in millivolts: %d", buf);
         }
 
         k_msleep(500);
     }
+
+    return 0;
 }
