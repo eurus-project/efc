8fa20e58ffc6e91b944548706af7dc581775f006
Filip Stojanovic <filipembedded@gmail.com>
2025-07-13 13:05:52 +0200

    demos(flash): Succesfully built flash demo - untested

--- demos/flash/src/main.c
+++ demos/flash/src/main.c
@@ -19,13 +19,12 @@
 #include <stdio.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
-#include <zephyr/drivers/sensor.h>
+#include <zephyr/fs/fs.h>
+#include <zephyr/fs/littlefs.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/usb/usb_device.h>
 #include <zephyr/usb/usbd.h>
-
-#include "types.h"
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
@@ -34,6 +33,18 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 // apparent without looking at log output or hooking up a debugger.
 static const struct gpio_dt_spec fw_running_led =
     GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+
+static struct fs_littlefs main_fs;
+
+struct fs_mount_t main_fs_mount = {
+    .type = FS_LITTLEFS,
+    .fs_data = &main_fs,
+    // .flags = FS_MOUNT_FLAG_USE_DISK_ACCESS,
+    .storage_dev = (void *)FIXED_PARTITION_ID(lfs_partition),
+    .mnt_point = "/SPI_FLASH:",
+};
+
+int32_t boot_count = -1;
 
 #if defined(CONFIG_USB_DEVICE_STACK_NEXT)
 static struct usbd_context *sample_usbd;
@@ -55,39 +66,76 @@ static int enable_usb_device_next(void) {
 }
 #endif /* defined(CONFIG_USB_DEVICE_STACK_NEXT) */
 
-static int process_baro(const struct device *dev) {
-    int ret = sensor_sample_fetch(dev);
+static int update_boot_count(void) {
+    struct fs_file_t boot_count_file;
+    fs_file_t_init(&boot_count_file);
+
+    char filename[LFS_NAME_MAX];
+    snprintf(filename, sizeof(filename), "%s/boot_count",
+             main_fs_mount.mnt_point);
+    int ret = fs_open(&boot_count_file, filename, FS_O_RDWR | FS_O_CREATE);
     if (ret < 0) {
-        LOG_ERR("Could not fetch data from barometer!");
+        LOG_ERR("Could not open file!\n");
         return ret;
     }
 
-    const uint32_t timestamp_ms = k_uptime_get();
-
-    struct sensor_value baro_temp;
-    ret = sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &baro_temp);
+    ret = fs_read(&boot_count_file, &boot_count, sizeof(boot_count));
     if (ret < 0) {
-        LOG_ERR("Could not get barometer temperature data!");
+        LOG_ERR("Could not read from file!\n");
         return ret;
     }
 
-    struct sensor_value baro_press;
-    ret = sensor_channel_get(dev, SENSOR_CHAN_PRESS, &baro_press);
+    LOG_INF("Boot count: %d\n", (int)boot_count);
+
+    ret = fs_seek(&boot_count_file, 0, FS_SEEK_SET);
     if (ret < 0) {
-        LOG_ERR("Could not get barometer pressure data!");
+        LOG_ERR("Could not seek to beggining of the file!\n");
         return ret;
     }
 
-    const struct baro_data msg = {
-        .timestamp_us = timestamp_ms * 1000,
-        .temperature_degc = sensor_value_to_float(&baro_temp),
-        .pressure_kpa = sensor_value_to_float(&baro_press),
-    };
+    boot_count += 1;
+    ret = fs_write(&boot_count_file, &boot_count, sizeof(boot_count));
+    if (ret < 0) {
+        LOG_ERR("Could not write to file!\n");
+        return ret;
+    }
 
-    printk("Baro Data:\n");
-    printk("  Timestamp: %llu us\n", msg.timestamp_us);
-    printk("  Temperature: %.2f °C\n", msg.temperature_degc);
-    printk("  Pressure: %.2f kPa\n", msg.pressure_kpa);
+    ret = fs_close(&boot_count_file);
+    if (ret < 0) {
+        LOG_ERR("Could not close file!\n");
+        return ret;
+    }
+
+    return 0;
+}
+
+static int create_demo_file(void) {
+    struct fs_file_t demo_file;
+    const char demo_data[] = "This is a demo test message from EFC";
+
+    fs_file_t_init(&demo_file);
+
+    char filename[LFS_NAME_MAX];
+    snprintf(filename, sizeof(filename), "%s/demo_file.txt",
+             main_fs_mount.mnt_point);
+
+    int ret = fs_open(&demo_file, filename, FS_O_RDWR | FS_O_CREATE);
+    if (ret < 0) {
+        LOG_ERR("Could not open file!\n");
+        return ret;
+    }
+
+    ret = fs_write(&demo_file, &demo_data, sizeof(demo_data));
+    if (ret < 0) {
+        LOG_ERR("Could not write to file!\n");
+        return ret;
+    }
+
+    ret = fs_close(&demo_file);
+    if (ret < 0) {
+        LOG_ERR("Could not close file!\n");
+        return ret;
+    }
 
     return 0;
 }
@@ -117,11 +165,19 @@ int main(void) {
         return 0;
     }
 
-    const struct device *const main_baro = DEVICE_DT_GET_ANY(bosch_bme280);
+    ret = fs_mount(&main_fs_mount);
+    if (ret < 0) {
+        LOG_ERR("Could not mount filesystem!\n");
+    }
 
-    if (!device_is_ready(main_baro)) {
-        printk("Device %s is not ready\n", main_baro->name);
-        return 0;
+    ret = update_boot_count();
+    if (ret < 0) {
+        LOG_ERR("Could not update boot count!");
+    }
+
+    ret = create_demo_file();
+    if (ret < 0) {
+        LOG_ERR("Could not create demo file!");
     }
 
     while (1) {
@@ -130,8 +186,6 @@ int main(void) {
             LOG_ERR("Could not toggle the firmware running LED");
             return 0;
         }
-
-        process_baro(main_baro);
 
         k_msleep(1000);
     }
