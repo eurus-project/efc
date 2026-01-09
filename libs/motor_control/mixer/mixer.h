7ac17354fe806334c3aa6c6e81703799e89e3841
Filip Stojanovic <filipembedded@gmail.com>
2026-01-09 22:55:23 +0100

    mixer: Added doxygen function headers

--- libs/motor_control/mixer/mixer.h
+++ libs/motor_control/mixer/mixer.h
@@ -63,10 +63,75 @@ typedef struct {
     bool initialized;
 } MIXER_Inst_Type;
 
-MIXER_Error_Type MIXER_AddMotor(MIXER_Inst_Type *mixer, ESC_Inst_Type *esc);
-
+/**
+ * @brief Mixer initialization function
+ * @param mixer Pointer to the mixer instance struct
+ * @param uav_cfg Geometrical drone configuration
+ *
+ * @retval MIXER_INIT_ERROR Error in initialization, wrong args
+ * @retval MIXER_INVALID_CFG Invalid or unsupported geometrical configuration
+ * @retval MIXER_SUCCESS Mixer initialization successfull
+ *
+ * @attention MIXER_Init must be called before MIXER_AddMotor call
+ */
 MIXER_Error_Type MIXER_Init(MIXER_Inst_Type *mixer, MIXER_UAV_Cfg_Type uav_cfg);
 
+/**
+ * @brief Function to instantiate specific motor
+ * @param mixer Pointer to the mixer instance struct
+ * @param esc Pointer to the esc instance struct
+ *
+ * @retval MIXER_INIT_ERROR Error in initialization, wrong args
+ * @retval MIXER_ESC_ERROR ESC not initialized correctly
+ * @retval MIXER_SUCCESS Mixer initialization succesfull
+ *
+ * @attention MIXER_AddMotor can be called only after MIXER_Init call
+ * @note The motor instances must be added at correct order, based on this
+ *       MIXER_UAV_CFG_QUADROTOR_X configuration:
+ *
+ *         FRONT
+ *      [3]     [1]
+ *         \   /
+ *          [ ]
+ *         /   \
+ *      [2]     [4]
+ *         BACK
+ *
+ *      Initialization flow should be done like in this code snippet:
+ *      MIXER_Init(&mixer, MIXER_UAV_CFG_QUADROTOR_X);
+ *
+ *      MIXER_AddMotor(&mixer, &esc1);
+ *      MIXER_AddMotor(&mixer, &esc2);
+ *      MIXER_AddMotor(&mixer, &esc3);
+ *      MIXER_AddMotor(&mixer, &esc4);
+ *
+ *      ,where:
+ *      - esc1 represents motor at position [1]
+ *      - esc2 represents motor at position [2]
+ *      - esc3 represents motor at position [3]
+ *      - esc4 represents motor at position [4]
+ * @note Currently only quadrotor x geometrical configuration has been supported
+ */
+MIXER_Error_Type MIXER_AddMotor(MIXER_Inst_Type *mixer, ESC_Inst_Type *esc);
+
+/**
+ * @brief Transforms pilot control inputs into motor commands through matrix
+ *        mixing, applies saturation protection, and outputs to ESC instances
+ * @param mixer Pointer to the mixer instance struct
+ * @param mixer_in Pointer to the input data struct. Input format:
+ *                 - thrust [0.0, 1.0]
+ *                 - roll [-1.0, 1.0]
+ *                 - pitch [-1.0, 1.0]
+ *                 - yaw [-1.0, 1.0]
+ *                 The provided values should be normalized and in specified
+ *                 range.
+ * @retval MIXER_INIT_ERROR Error in initialization, wrong args or insuficient
+ *                          motors has been initialized based on chosen config
+ * @retval MIXER_OUT_OF_BOUND_VALS Input values exceed predefined bounds
+ * @retval MIXER_ESC_ERROR ESC could not execute motor output set
+ * @retval MIXER_SUCCESS Mixer executed successfully
+ *
+ */
 MIXER_Error_Type MIXER_Execute(MIXER_Inst_Type *mixer,
                                const MIXER_Input_Type *mixer_in);
 
